eb3e60f57ccc82db98aecd735863917d76fae933
zynmurel <comingues99@gmail.com>
2024-11-09 12:37:38 +0800

    setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,6 @@ import { LatestPost } from "@/app/_components/post";
 import { api, HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
-  void api.post.getLatest.prefetch();
-
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
@@ -40,9 +36,7 @@ export default async function Home() {
             </Link>
           </div>
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
+            <p className="text-2xl text-white"></p>
           </div>
 
           <LatestPost />
