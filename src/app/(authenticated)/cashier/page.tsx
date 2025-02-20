dcd1648f698fe522301f9d9a8b233511a2b6d14f
Jae Yaon <yaon.jae.2001@gmail.com>
2025-02-20 13:59:31 +0800

    receipt modification

--- src/app/(authenticated)/cashier/page.tsx
+++ src/app/(authenticated)/cashier/page.tsx
@@ -282,7 +282,7 @@ const Cashier = () => {
       } else {
         const fullName = customerName?.first_name + " " + customerName?.last_name;
         textData = [
-          "\n             ",
+          "\n",
           `${centerText("Canteen Payment")}\n`,
           `${centerText("Management System")}\n\n`,
           `Date: ${getFormattedDateDay()}\n`,
